e5787130d417bfb3439f3731f292aef0cfa47ecd
Darren Ogola <darren.sunday@strathmore.edu>
2024-12-01 22:36:20 +0300

    My Day37 in 365 challange

---
+++ Day37.py
@@ -0,0 +1,5 @@
+ from difflib import SequenceMatcher
+ text1=input("Enter 1st sentence:")
+ text2=input("Enter 2nd sentence:")
+ sequenceScore=SequenceMatcher(None,text1,text2).ratio()
+ print(f"Both are {sequenceScore*100} %similar")
